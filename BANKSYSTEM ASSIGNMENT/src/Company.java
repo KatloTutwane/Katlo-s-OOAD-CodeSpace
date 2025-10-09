63b6eaf60850d7f89f4a8d0c9630cad814f5654a
Katlo Tutwane <cse24-097@thuto.bac.ac.bw>
2025-10-09 17:25:31 +0200

    init commit

---
+++ BANKSYSTEM ASSIGNMENT/src/Company.java
@@ -0,0 +1,27 @@
+
+public class Company extends Customer {
+    private String companyName;
+    private String companyAddress;
+
+    public Company(String companyName, String companyAddress,
+                   String contactFirstName, String contactLastName,
+                   String email, String address, String userName, String password) {
+        super(contactFirstName, contactLastName, email, address, userName, password);
+        this.companyName = companyName;
+        this.companyAddress = companyAddress;
+    }
+
+    public String getCompanyName() { return companyName; }
+    public String getCompanyAddress() { return companyAddress; }
+
+    @Override
+    public void openAccount(Account account) {
+        System.out.println("Opening company account for " + companyName);
+        addAccount(account);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Company: %s (Contact: %s %s)", companyName, firstName, lastName);
+    }
+}
---
+++ BANKSYSTEM ASSIGNMENT/src/Customer.java
@@ -0,0 +1,54 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+
+public abstract class Customer {
+    protected final String custId;
+    protected String firstName;
+    protected String lastName;
+    protected String email;
+    protected String address;
+    protected String userName;
+    protected String password;
+
+    protected final List<Account> accounts = new ArrayList<>();
+
+    public Customer(String firstName, String lastName, String email, String address, String userName, String password) {
+        this.custId = UUID.randomUUID().toString();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.address = address;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getCustId() { return custId; }
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+    public String getEmail() { return email; }
+    public String getAddress() { return address; }
+    public String getUserName() { return userName; }
+    public String getPassword() { return password; }
+
+    public void setEmail(String email) { this.email = email; }
+    public void setAddress(String address) { this.address = address; }
+
+    public List<Account> getAccounts() {
+        return new ArrayList<>(accounts);
+    }
+
+    public void addAccount(Account account) {
+        if (account != null && !accounts.contains(account)) accounts.add(account);
+    }
+
+
+    public abstract void openAccount(Account account);
+
+    @Override
+    public String toString() {
+        return String.format("%s %s (%s)", firstName, lastName, getClass().getSimpleName());
+    }
+}
